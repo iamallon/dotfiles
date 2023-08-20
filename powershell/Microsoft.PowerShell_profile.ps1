e9d5db39f2a5d436d43aaefc6c00342aab0e3937
Loghin Alexandru <loghinalexandru61@gmail.com>
2023-08-20 16:15:23 +0300

    Add 'vs' command

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,10 @@
-Set-Alias -Name ls -Value Get-ChildItem
-Set-Alias -Name grep -Value Select-String
-Set-Alias -Name touch -Value New-Item
-Set-Alias -Name rm -Value Remove-Item
-Set-Alias -Name mv -Value Move-Item
-Set-Alias -Name cp -Value Copy-Item
-Set-Alias -Name pwd -Value Get-Location
+Set-Alias -Name ls -Value Get-ChildItem -Option AllScope
+Set-Alias -Name grep -Value Select-String -Option AllScope
+Set-Alias -Name touch -Value New-Item -Option AllScope
+Set-Alias -Name rm -Value Remove-Item -Option AllScope
+Set-Alias -Name mv -Value Move-Item -Option AllScope
+Set-Alias -Name cp -Value Copy-Item -Option AllScope
+Set-Alias -Name pwd -Value Get-Location -Option AllScope
+Set-Alias -Name vs -Value "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" -Option AllScope
 
 Invoke-Expression (&starship init powershell)
