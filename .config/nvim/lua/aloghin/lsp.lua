e970c9000a4e0e18c019b030b4772087deceb50c
Loghin Alexandru <loghinalexandru61@gmail.com>
2025-02-15 12:10:37 +0200

    Add more QOL bindings

--- .config/nvim/lua/aloghin/lsp.lua
+++ .config/nvim/lua/aloghin/lsp.lua
@@ -13,6 +13,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
         vim.keymap.set("n", "gf", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "ff", function() vim.lsp.buf.format() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "<C-Up>", function() vim.diagnostic.goto_prev() end, opts)
