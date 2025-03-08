b9fc8a4c679cd20df5db2dce002e02ecf9799b72
a.loghin <alex.loghin@outlook.com>
2025-03-08 15:35:46 +0200

    Tweak oil plugin

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,10 +2,15 @@ return
 {
     "stevearc/oil.nvim",
     tag = "v2.14.0",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    },
+    lazy = false,
     config = function()
         require("oil").setup {
             default_file_explorer = true,
             columns = {
+                "icon",
                 "size"
             },
             view_options = {
