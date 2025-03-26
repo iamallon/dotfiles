536c8523678c5d420af8ab7f21c8bd5e050a99e3
Loghin Alexandru <loghinalexandru61@gmail.com>
2025-03-26 22:14:03 +0200

    feat: add black hole register remap for delete

--- .config/nvim/lua/aloghin/keymap.lua
+++ .config/nvim/lua/aloghin/keymap.lua
@@ -6,6 +6,9 @@ vim.keymap.set("n", "<space>b", require("telescope.builtin").buffers)
 vim.keymap.set("n", "<space>g", require("telescope.builtin").live_grep)
 vim.keymap.set("n", "<space>w", function() require("telescope.builtin").grep_string({search = vim.fn.expand("<cword>")}) end)
 
+vim.keymap.set("n", "d", "\"_d")
+vim.keymap.set("n", "dd", "\"_dd")
+
 vim.keymap.set("o", "iq", "i\"")
 vim.keymap.set("o", "iQ", "i\'")
 vim.keymap.set("o", "aq", "a\"")
