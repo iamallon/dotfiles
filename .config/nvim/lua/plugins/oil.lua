2c7d54a80f525acacf9bea5a67eb4ab76265ec5e
Loghin Alexandru <loghinalexandru61@gmail.com>
2025-04-05 20:41:59 +0300

    feat: change theme & remove stupid icons

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,16 +2,12 @@ return
 {
     "stevearc/oil.nvim",
     tag = "v2.14.0",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons"
-    },
     lazy = false,
     config = function()
         require("oil").setup {
             default_file_explorer = true,
             columns = {
-                "icon",
-                "size"
+              "size"
             },
             view_options = {
                 show_hidden = true,
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,53 +1,17 @@
 return
 {
-    "AlexvZyl/nordic.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("nordic").setup {
-            on_palette = function(palette)
-                palette.red.base = palette.blue0
-                palette.red.bright = palette.blue1
-                palette.red.dim = palette.blue2
-                palette.orange.base = palette.blue0
-                palette.orange.bright = palette.blue1
-                palette.orange.dim = palette.blue2
-                palette.yellow.base = palette.white0
-                palette.yellow.bright = palette.white1
-                palette.yellow.dim = palette.white2
-            end,
-            on_highlight = function(highlights, palette)
-                local U = require("nordic.utils")
-                highlights.Search = { bg = palette.bg_visual, fg = palette.yellow.bright, bold = true, underline = false }
-                highlights.MatchParen = { bg = U.blend(palette.gray5, palette.bg, 0.50) }
-                highlights.LineNrAbove = { fg = palette.gray5, bold = false }
-                highlights.LineNr = { fg = palette.white0, bold = true }
-                highlights.LineNrBelow = { fg = palette.gray5, bold = false }
-                highlights.TelescopeSelection = { bg = U.blend(palette.gray5, palette.bg, 0.50) }
-                highlights.TelescopeSelectionCaret = { fg = palette.bg, bg = palette.bg }
-                highlights.TelescopeMultiSelection = { bold = true }
-                for _, highlight in pairs(highlights) do
-                    highlight.italic = false
-                end
-            end,
-            after_palette = function(palette)
-                local U = require("nordic.utils")
-                palette.bg_visual = U.blend(palette.gray5, palette.bg, 0.50)
-                palette.bg = palette.black1
-                palette.bg_float = "#000000"
-                palette.comment = palette.gray5
-            end,
-            bold_keywords = true,
-            reduced_blue = false,
-            telescope = {
-                style = "classic"
-            },
-            transparent = {
-                bg = false,
-                float = false,
-            }
-        }
+  "miikanissi/modus-themes.nvim",
+  priority = 1000,
+  config = function()
+    require("modus-themes").setup{
+      variant = "default",
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false }
+      }
+    }
 
-        vim.cmd.colorscheme("nordic")
-    end
+    vim.cmd([[colorscheme modus_vivendi]])
+  end
 }
+
