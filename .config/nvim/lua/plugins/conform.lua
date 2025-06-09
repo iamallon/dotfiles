d7766f4e3f0677b7bab7309a94e2d589e10d52e8
Alexandru Loghin <contact@allon.dev>
2025-06-09 18:47:00 +0300

    feat: add typos-lsp & more formatters

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,17 +1,21 @@
 return
 {
-  'stevearc/conform.nvim',
-  config = function()
-    require("conform").setup({
-      default_format_opts = {
-        lsp_format = "first",
-        stop_after_first = true,
-        async = true
-      },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        cs = { "csharpier" }
-      }
-    })
-  end
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			default_format_opts = {
+				lsp_format = "first",
+				stop_after_first = true,
+				async = true,
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				cs = { "csharpier" },
+				html = { "superhtml" },
+				css = { "biome" },
+				js = { "biome" },
+				json = { "biome" },
+			},
+		})
+	end,
 }
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,11 +1,14 @@
 return
 {
-  "neovim/nvim-lspconfig",
-  tag = "v1.6.0",
-  config = function()
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    require("lspconfig").gopls.setup {
-      capabilities = capabilities
-    }
-  end
+	"neovim/nvim-lspconfig",
+	tag = "v1.6.0",
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		require("lspconfig").gopls.setup({
+			capabilities = capabilities,
+		})
+		require("lspconfig").typos_lsp.setup({
+			capabilities = capabilities,
+		})
+	end,
 }
