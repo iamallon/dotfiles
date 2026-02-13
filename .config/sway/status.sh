4d36ccf9fe6fccabdfd67fe32fb90a499f34446e
Alexandru Loghin <contact@allon.dev>
2026-02-13 18:26:40 +0200

    feat: edit sway/status config

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 uptime_formatted=$(uptime | cut -d ',' -f1  | cut -d ' ' -f4,5)
-date_formatted=$(date "+%a %F %H:%M %X")
-battery_status=$(cat /sys/class/power_supply/BAT1/status)
+date_formatted=$(date "+%a %F %H:%M:%S")
+battery_internal_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+battery_external_capacity=$(cat /sys/class/power_supply/BAT1/capacity)
 
-echo "$uptime_formatted | $battery_status | $date_formatted"
+echo "$uptime_formatted | ${battery_internal_capacity:=N/A}% | ${battery_external_capacity:=N/A}% | $date_formatted"
